f3f9f8a9d92c065dea347e60a3b1ea1381696341
Ivan Toropygin <itori86@mail.ru>
2025-06-27 16:48:17 +0300

    RootComponent. Navigation

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -1,34 +1,25 @@
 package com.example.weatherapp.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.weatherapp.data.network.api.ApiFactory
-import com.example.weatherapp.presentation.ui.theme.WeatherAppTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.arkivanov.decompose.defaultComponentContext
+import com.example.weatherapp.WeatherApp
+import com.example.weatherapp.presentation.root.DefaultRootComponent
+import com.example.weatherapp.presentation.root.RootContent
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var rootComponentFactory: DefaultRootComponent.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        (applicationContext as WeatherApp).applicationComponent.inject(this)
         super.onCreate(savedInstanceState)
 
-        val apiService = ApiFactory.apiService
-        CoroutineScope(Dispatchers.IO).launch {
-            val current = apiService.loadCurrentWeather("London")
-            val forecast = apiService.loadForecastWeather("London")
-            val search = apiService.searchCity("London")
-
-            Log.d("MainActivityWeather", "current: $current")
-            Log.d("MainActivityWeather", "forecast: $forecast")
-            Log.d("MainActivityWeather", "search: $search")
-        }
-
         setContent {
-            WeatherAppTheme {
-
-            }
+            RootContent(component = rootComponentFactory.create(defaultComponentContext()))
         }
     }
 }
--- app/src/main/java/com/example/weatherapp/presentation/root/DefaultComponent.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weatherapp.presentation.root
-
-import com.arkivanov.decompose.ComponentContext
-
-class DefaultComponent(
-    componentContext: ComponentContext
-) : RootComponent, ComponentContext by componentContext

4914c3d3a02e77d31b37894286bca963a00bf2df
Ivan Toropygin <itori86@mail.ru>
2025-06-29 16:53:35 +0300

    FavouriteContent

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.arkivanov.decompose.defaultComponentContext
 import com.example.weatherapp.WeatherApp
+import com.example.weatherapp.domain.usecase.ChangeFavouriteStateUseCase
+import com.example.weatherapp.domain.usecase.SearchCityUseCase
 import com.example.weatherapp.presentation.root.DefaultRootComponent
 import com.example.weatherapp.presentation.root.RootContent
 import javax.inject.Inject
@@ -14,8 +16,15 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var rootComponentFactory: DefaultRootComponent.Factory
 
+    @Inject
+    lateinit var searchCityUseCase: SearchCityUseCase
+
+    @Inject
+    lateinit var changeFavouriteStateUseCase: ChangeFavouriteStateUseCase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as WeatherApp).applicationComponent.inject(this)
+
         super.onCreate(savedInstanceState)
 
         setContent {
